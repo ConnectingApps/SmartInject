df85bd08ea70639b1e39e61b3975a26da8d34a08
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-02 18:44:02 +0200

    Refactored to modern C# style

--- ConnectingApps.SmartInject/LazyServicesExtensions.cs
+++ ConnectingApps.SmartInject/LazyServicesExtensions.cs
@@ -1,32 +1,31 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using System;
 
-namespace ConnectingApps.SmartInject
+namespace ConnectingApps.SmartInject;
+
+public static class LazyServicesExtensions
 {
-    public static class LazyServicesExtensions
+    public static IServiceCollection AddLazyTransient<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
     {
-        public static IServiceCollection AddLazyTransient<TService, TImplementation>(this IServiceCollection services)
-            where TService : class
-            where TImplementation : class, TService
-        {
-            return services.AddTransient<TService, TImplementation>()
-                .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
-        }
+        return services.AddTransient<TService, TImplementation>()
+            .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
+    }
 
-        public static IServiceCollection AddLazySingleton<TService, TImplementation>(this IServiceCollection services)
-            where TService : class
-            where TImplementation : class, TService
-        {
-            return services.AddSingleton<TService, TImplementation>()
-                .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
-        }
+    public static IServiceCollection AddLazySingleton<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        return services.AddSingleton<TService, TImplementation>()
+            .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
+    }
 
-        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection services)
-            where TService : class
-            where TImplementation : class, TService
-        {
-            return services.AddSingleton<TService, TImplementation>()
-                .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
-        }
+    public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection services)
+        where TService : class
+        where TImplementation : class, TService
+    {
+        return services.AddSingleton<TService, TImplementation>()
+            .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
     }
 }

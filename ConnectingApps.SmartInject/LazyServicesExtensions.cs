3e5c2aba870a9dae6696d732997d33c3c9c5e27c
Daan Acohen <daan.acohen@connectingapps.net>
2023-10-02 18:41:09 +0200

    Three types of lazy service injection

--- ConnectingApps.SmartInject/LazyServicesExtensions.cs
+++ ConnectingApps.SmartInject/LazyServicesExtensions.cs
@@ -12,5 +12,21 @@ namespace ConnectingApps.SmartInject
             return services.AddTransient<TService, TImplementation>()
                 .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
         }
+
+        public static IServiceCollection AddLazySingleton<TService, TImplementation>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            return services.AddSingleton<TService, TImplementation>()
+                .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
+        }
+
+        public static IServiceCollection AddScoped<TService, TImplementation>(this IServiceCollection services)
+            where TService : class
+            where TImplementation : class, TService
+        {
+            return services.AddSingleton<TService, TImplementation>()
+                .AddTransient(a => new Lazy<TService>(a.GetRequiredService<TService>));
+        }
     }
 }
